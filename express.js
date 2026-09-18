1208138dd02c251454b08c8eb123147eaba5bf12
Nicolas <nicmann657@gmail.com>
2026-09-19 01:33:48 +0200

    Express Ejs

---
+++ express.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 80880;
+
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+
+let users=[
+    {id:1,name:'Bolton',age:40},
+    {id:2,name:'Adeyemi',age:22},
+    {id:3,name:'Kayode',age:35},
+    {id:4,name:'Toni',age:100},
+];
+
+app.get('/',(req,res) => {
+    res.render('index',{users});
+})
+app.listen(port,()=> {
+    console.log('Welcome to Backend')
+})
